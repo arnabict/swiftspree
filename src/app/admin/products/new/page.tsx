ff41a17d9ed21913f243597ae4cf1a1b56be8bf5
Arnab Paul <arnabict@outlook.com>
2026-01-09 16:06:54 +0100

    feat: product form created

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../../_components/PageHeader";
+import { ProductForm } from "../_components/ProductFrom";
+
+export default function NewProductPage() {
+  return (
+    <>
+      <PageHeader>Add Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+}
