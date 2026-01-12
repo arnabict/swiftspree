bca0b6a042a640b00036a99f777569c4ed053404
Arnab Paul <arnabict@outlook.com>
2026-01-12 15:50:39 +0100

    feat: update product

--- src/app/admin/_components/ProductActions.tsx
+++ src/app/admin/_components/ProductActions.tsx
@@ -3,6 +3,7 @@
 import { useTransition } from "react";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { deleteProduct, toggleProductActivity } from "../_actions/products";
+import { useRouter } from "next/navigation";
 
 export function ActiveToggleDropdownItem({
   id,
@@ -12,12 +13,14 @@ export function ActiveToggleDropdownItem({
   isAvailableForPurchase: boolean;
 }) {
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       disabled={isPending}
       onClick={() => {
         startTransition(async () => {
           await toggleProductActivity(id, !isAvailableForPurchase);
+          router.refresh();
         });
       }}
     >
@@ -34,12 +37,14 @@ export function DeleteDropdownItem({
   disabled: boolean;
 }) {
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <DropdownMenuItem
       disabled={disabled || isPending}
       onClick={() => {
         startTransition(async () => {
           await deleteProduct(id);
+          router.refresh();
         });
       }}
       className="text-red-600 focus:text-red-50 focus:bg-red-600 hover:bg-red-600 hover:text-red-50"
--- src/app/admin/products/_components/ProductFrom.tsx
+++ src/app/admin/products/_components/ProductFrom.tsx
@@ -6,18 +6,31 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useActionState, useState } from "react";
-import { addProduct } from "../../_actions/products";
+import { addProduct, updateProduct } from "../../_actions/products";
 import { useFormStatus } from "react-dom";
+import { Product } from "@/generated/prisma";
+import Image from "next/image";
 
-export function ProductForm() {
-  const [error, action] = useActionState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number | "">("");
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, action] = useActionState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {}
+  );
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(
+    product?.priceInCents ?? undefined
+  );
 
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -27,10 +40,8 @@ export function ProductForm() {
           id="priceInCents"
           name="priceInCents"
           required
-          value={priceInCents}
-          onChange={(e) =>
-            setPriceInCents(e.target.value === "" ? "" : Number(e.target.value))
-          }
+          value={priceInCents ?? ""}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
         {error.priceInCents && (
           <div className="text-destructive">{error.priceInCents}</div>
@@ -43,19 +54,30 @@ export function ProductForm() {
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description || ""}
+        />
         {error.description && (
           <div className="text-destructive">{error.description}</div>
         )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+        {product != null && (
+          <div className="text-muted-foreground">{product.filePath}</div>
+        )}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product != null && (
+          <Image src={product.imagePath} alt="Product Image" />
+        )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
